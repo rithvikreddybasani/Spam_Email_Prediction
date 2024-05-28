7cd50038c911738ab8af0986bcc2e80372678a36
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-05-28 17:14:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open("C:\\Users\\Risathvik\\OneDrive\\Desktop\\saved models\\vectorizer.pkl",'rb'))
-model = pickle.load(open("C:\\Users\\Risathvik\\OneDrive\\Desktop\\saved models\\model.pkl",'rb'))
+tfidf = pickle.load(open("vectorizer.pkl",'rb'))
+model = pickle.load(open("model.pkl",'rb'))
 
 st.title("Email/SMS Spam Classifier")
 
